25a4aa93bef1c7a528913f845e9ea54682985a79
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2022-02-17 13:39:10 +0100

    Redirect to login page if no authenticated

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Header from './components/Header';
 import Home from './components/Home/Home';
 import Detail from './components/Detail';
 import Login from './components/Login/Login';
-
+import { auth, provider } from "./firebase";
+import { selectUserName, selectPhoto, selectEmail, setUserLogin, setSignOut } from './features/user/userSlice'
+import { useSelector } from 'react-redux';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
+  Navigate
 } from "react-router-dom";
 import Footer from './components/Footer';
 
 function App() {
+  const userName = useSelector(selectUserName);
+  let isAuthenticated;
+
+  (userName != null && userName !== "") ? isAuthenticated = true : isAuthenticated = false;
   return (
     <div className="App">
       <Router>
@@ -22,7 +28,9 @@ function App() {
 
           <Route path="/login" element={<Login />}></Route>
           <Route path="/detail/:id" element={<Detail />}> </Route>
-          <Route path="/" element={<Home />}></Route>
+          {userName}
+          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate replace to="/login" />}>
+          </Route>
 
         </Routes >
         <Footer />
